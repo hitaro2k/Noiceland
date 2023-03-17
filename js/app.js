4f8bb7fcb5687c5ebb3a52f54dd1f8dbfa8199b1
Vlad Bojenko <vlad.bojenko2005@gmail.com>
2023-03-18 01:00:23 +0200

    add full adaptive main page and page post

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+window.JustValidate
 document.addEventListener("DOMContentLoaded", function(){
-    
+    const validator = new JustValidate(".footer-form",{
+        validateBeforeSubmitting: true,
+    });
+    validator.addField('#async_email', [
+    {
+        rule: 'required',
+    },
+    {
+        validator: (value) => () =>
+        new Promise((resolve) => {
+            setTimeout(() => {
+            resolve(false);
+            }, 1000);
+        }),
+        errorMessage: 'Email already exists!',
+    },
+    ]);
+
+
+    // ! Механизировать создание статей
 })
